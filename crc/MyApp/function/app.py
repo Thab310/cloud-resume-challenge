e8ecbb82a956bac90a965380a9ff7b338bf7c433
Thabelo Ramabulana <th9b310@gmail.com>
2022-12-15 21:49:31 +0200

    Added an AWS region to app.py

--- crc/MyApp/function/app.py
+++ crc/MyApp/function/app.py
@@ -1,7 +1,7 @@
 import json
 import boto3
 
-dynamodb = boto3.resource('dynamodb')
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
 table = dynamodb.Table('CRC-table')
 
 def lambda_handler(event, context):
